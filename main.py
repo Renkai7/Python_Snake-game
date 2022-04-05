0dcf95d6413ec295169f618dda2639a84ed1c474
Mike <ms53150@gmail.com>
2022-04-05 14:16:02 -0400

    create loop to initialize snake

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from turtle import Screen, Turtle
 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+all_turtles = []
+x_index = 0
+
+# initialize snake
+for turtle_index in range(3):
+    new_turtle = Turtle("square")
+    new_turtle.color("white")
+    new_turtle.goto(x=x_index, y=0)
+    x_index += -20
+    all_turtles.append(new_turtle)
+
+
+screen.exitonclick()
