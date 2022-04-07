8e362454eac165b67beb81ecd8f926065f57dbb3
Mike <ms53150@gmail.com>
2022-04-07 02:32:41 -0400

    add scoreboard class to increase score upon eating food

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
+
 import time
 
 screen = Screen()
@@ -11,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 # listen for keystrokes
 screen.listen()
@@ -28,9 +31,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
