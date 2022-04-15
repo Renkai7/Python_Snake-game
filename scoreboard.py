481d56af2982ce45ed2396530afd4dc481770447
Mike <ms53150@gmail.com>
2022-04-15 19:22:05 -0400

    Set high score to be read from data.txt file

--- scoreboard.py
+++ scoreboard.py
@@ -13,24 +13,23 @@ class Scoreboard(Turtle):
         self.penup()
         self.setposition(0, 270)
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    # reset and check for high score
+    # reset and check for high scores
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-
+        # writes current high score to data.txt to save high score
+        with open("data.txt", mode="w") as file:
+            file.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
-
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
